f9f2798e0d46d3e6bb5f7255a5c0dd14dca2afff
gitman <https://github.com/gitman>
2013-01-17 22:18:40 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NflDraft::Application.config.secret_token = '7727510e35dcac8a76c2a793ca32aacb6549e14e2a8e09c4a757fb851353e950ced323a5f491fb83cc7c6fb89374b187adc9d0af85017b4d227c24ebd58e6b3f'
